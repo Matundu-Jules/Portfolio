9f239e82629a220a2ce2f157e2802b59cc7fac88
PereCastorrr <matundu.pro@gmail.com>
2021-01-11 17:15:48 +0100

    Ajout de la page d'accueil, Ajout du changement de photo sur le hover des btn, Animation en fin de chargement.

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 $(function () {
+  //Animation Boutons Page d'accueil :
+  $("#about").animate({ opacity: 1.0 }, 2000);
+
+  //Animation lorsque l'on passe la souris sur un btn de l'accueil:
+  $("#sectionAnim").hover(
+    function () {
+      // over
+      $("img").fadeOut(1);
+      $("img").attr("src", "img/me3.jpg");
+      $("img").fadeIn(1000);
+    },
+    function () {
+      // out
+    }
+  );
+  $("#sectionDev").hover(
+    function () {
+      // over
+      $("img").fadeOut(1);
+      $("img").attr("src", "img/me2.jpg");
+      $("img").fadeIn(1000);
+    },
+    function () {
+      // out
+    }
+  );
+  // Animation au click d'un lien :
   $(".navbar a, footer a").on("click", function (event) {
     event.preventDefault();
     var hash = this.hash;
