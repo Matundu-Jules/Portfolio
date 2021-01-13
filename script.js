84cfa1235440dda1730de7be16748ca155e05866
PereCastorrr <matundu.pro@gmail.com>
2021-01-13 18:29:03 +0100

    -Correction bug changement photos sur le hover des btn. -Ajout du plugin Animsition pour les animations de transition de page.

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
 $(function () {
-  //Animation Boutons Page d'accueil :
-  $("#about").animate({ opacity: 1.0 }, 2000);
+  //Animsition : Animation de transition entre les pages :
+  $(".animsition").animsition({
+    inClass: "fade-in-right",
+    outClass: "fade-out-right",
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: ".animsition-link",
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: true,
+    loadingParentElement: "body", //animsition wrapper element
+    loadingClass: "animsition-loading",
+    loadingInner: "", // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: ["animation-duration", "-webkit-animation-duration"],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay: false,
+    overlayClass: "animsition-overlay-slide",
+    overlayParentElement: "body",
+    transition: function (url) {
+      window.location.href = url;
+    },
+  });
 
-  //Animation lorsque l'on passe la souris sur un btn de l'accueil:
-  $(".btnAccueilAnim").hover(
-    function () {
-      // over
-      $(".imgCircleAccueil").fadeOut(1);
-      $(".imgCircleAccueil").attr("src", "img/me3.jpg");
-      $(".imgCircleAccueil").fadeIn(1000);
-    },
-    function () {
-      // out
+  // Variables :
+  let imgCircle;
+  let imgCircleAnim = $(".imgCircleAccueil").attr("src", "img/me3.jpg");
+  let imgCircleDev = $(".imgCircleAccueil").attr("src", "img/me2.jpg");
+
+  //Animation changement photo lorsque l'on passe la souris sur un btn de l'accueil:
+  function animationImgAnim() {
+    $(".imgCircleAccueil").fadeOut(0);
+    $(".imgCircleAccueil").attr("src", "img/me3.jpg");
+    $(".imgCircleAccueil").fadeIn(1000);
+    imgCircle = imgCircleAnim;
+  }
+  function animationImgDev() {
+    imgCircle.fadeOut(0);
+    $(".imgCircleAccueil").attr("src", "img/me2.jpg");
+    imgCircle.fadeIn(1000);
+    imgCircle = imgCircleDev;
+  }
+
+  $(".btnAccueilAnim").hover(function () {
+    // over
+    if (imgCircle != imgCircleAnim) {
+      animationImgAnim();
     }
-  );
-  $(".btnAccueilDev").hover(
-    function () {
-      // over
-      $(".imgCircleAccueil").fadeOut(1);
-      $(".imgCircleAccueil").attr("src", "img/me2.jpg");
-      $(".imgCircleAccueil").fadeIn(1000);
-    },
-    function () {
-      // out
+  });
+
+  $(".btnAccueilDev").hover(function () {
+    // over
+    if (imgCircle != imgCircleDev) {
+      animationImgDev();
     }
-  );
-  // Animation au click d'un lien :
+  });
+
+  // Animation au click d'un lien pour changer de catégorie :
   $(".navbar a, footer a").on("click", function (event) {
     event.preventDefault();
     var hash = this.hash;
